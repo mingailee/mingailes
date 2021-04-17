be47bcda2706d2a8e02128b2efabe91108234ecc
mingailee <mingaile.beinoryte@gmail.com>
2021-04-17 19:58:37 +0300

    Paspaudus mygtuka pasikeicia tekstas

--- app/src/main/java/com/example/pirmasmingailesdarbas/MainActivity.java
+++ app/src/main/java/com/example/pirmasmingailesdarbas/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.pirmasmingailesdarbas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain=findViewById(R.id.tvMain);
+    }
+    public void OnBtnclick(View view){
+        this.tvMain.setText("Keičiam teksta");
     }
 }
