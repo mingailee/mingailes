9ebf69e7aa1937262b80a6cc7e59505ad6ea1332
mingailee <mingaile.beinoryte@gmail.com>
2021-04-17 20:34:23 +0300

    Color changed

--- app/src/main/java/com/example/pirmasmingailesdarbas/MainActivity.java
+++ app/src/main/java/com/example/pirmasmingailesdarbas/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.pirmasmingailesdarbas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
 
+    Button Btncolor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.tvMain=findViewById(R.id.tvMain);
+        this.Btncolor=findViewById(R.id.Btncolor);
     }
     public void OnBtnclick(View view){
         this.tvMain.setText(R.string.changed_label_captions);
+    }
+    public void OnBtncolor(View view) {
+        this.Btncolor.setText("Spalva pasikeitė");
+        this.Btncolor.setTextColor(Color.GREEN);
+        this.tvMain.setTextColor(Color.RED);
     }
 }
